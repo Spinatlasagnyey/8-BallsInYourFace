c4a35906d06992f9340eb43fea2200811c3fe743
Linus <129332073+Spinatlasagnyey@users.noreply.github.com>
2026-04-18 18:20:33 +0200

    Navmesh agent für gegner

--- Enemies/Scripts/enemy_idle_state.gd
+++ Enemies/Scripts/enemy_idle_state.gd
@@ -1,17 +1,19 @@
 extends EnemyState
 
 @export var acceleration: float = 1000
+@export var agent: NavigationAgent2D
 
 func on_state_start():
-	pass
+	agent.target_position = player.global_position
 
 func update(delta: float) -> void:
-	pass
+	agent.target_position = player.global_position
 
 func fixed_update(delta: float) -> void:
-	entity.look_at(player.position)
+	var next_position = agent.get_next_path_position()
+	entity.look_at(global_position + next_position)
 	if(max_speed > entity.linear_velocity.length()): 
-		entity.apply_force((player.position - entity.position).normalized() * acceleration)
+		entity.apply_force((next_position - entity.position).normalized() * acceleration)
 	if not entity.get_colliding_bodies().is_empty():
 		state_manager.change_state(stunned_state)
 
