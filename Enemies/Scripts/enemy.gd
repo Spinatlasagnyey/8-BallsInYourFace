ea6e8c7233a1fb963390f0b41d84a76e70af80c0
Simon Markiewicz <simon.markiewicz@gmx.de>
2026-04-19 13:47:00 +0200

    LEVELPUSH

--- Enemies/Scripts/enemy.gd
+++ Enemies/Scripts/enemy.gd
@@ -1,8 +1,13 @@
 class_name Enemy
 extends RigidBody2D
 
+@export var textures: Array[CompressedTexture2D] 
+@export var sprite: Sprite2D
+
 func _ready() -> void:
 	call_deferred("add_to_game_state")
+	sprite.texture = textures[randi_range(0, textures.size() - 1)]
+
 
 func add_to_game_state():
 	GameState.enemies.append(self)
@@ -10,3 +15,8 @@ func add_to_game_state():
 
 func remove_from_game_state():
 	GameState.enemies.erase(self)
+	
+	
+func apply_random_color():
+	print("elo")
+	
